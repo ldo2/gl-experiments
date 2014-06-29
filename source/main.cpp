b2e419ec25386d1d6dde077f102deffe1dc40fdc
Levchenko Denis <ldo2.msiu@gmail.com>
2014-06-29 17:01:55 +0400

    distorted sphere

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <random>
+
 #include <cmath>
 
 #include "GlutApplication.hpp"
@@ -120,6 +122,91 @@ protected:
 	}
 };
 
+class DistortedSphereBuilder : public CubeBuilder {
+public:
+	DistortedSphereBuilder(int width, int height) : 
+		CubeBuilder(width, height), theDistortionMap(nullptr), 
+		theMapWidth(width + 2), theMapHeight(height + 2)
+	{
+		theDistortionMap = new GLfloat [theMapWidth * theMapHeight];
+		
+		std::default_random_engine generator;
+		std::uniform_real_distribution<GLfloat> distribution(0.6, 1.0);
+		
+		for (int i =0; i < theMapWidth; ++i) {
+			for (int j = 0; j < theMapHeight; ++j) {
+				theDistortionMap[i*theMapHeight + j] = distribution(generator);
+			}
+		}
+		
+		for (int s = 0; s < 16; ++s) {
+		for (int i = 1; i < theMapWidth - 1; ++i) {
+			for (int j = 1; j < theMapHeight - 1; ++j) {
+				theDistortionMap[i*theMapHeight + j] = 0.2 * (
+					theDistortionMap[i*theMapHeight + j] + 
+					theDistortionMap[i*theMapHeight + j + 1] + 
+					theDistortionMap[i*theMapHeight + j - 1] + 
+					theDistortionMap[(i + 1)*theMapHeight + j] + 
+					theDistortionMap[(i - 1)*theMapHeight + j]
+				);
+			}
+		}
+		}
+	}
+	
+	virtual ~DistortedSphereBuilder() {
+		delete[] theDistortionMap;
+	}
+	
+	DistortedSphereBuilder(const DistortedSphereBuilder&) = delete;
+	DistortedSphereBuilder& operator =(const DistortedSphereBuilder&) = delete;
+	
+protected:
+	virtual void evalVertex(GLfloat (&vertex)[4], GLfloat x, GLfloat y) {
+		int i(x*(theMapWidth - 3) + 1), j(y*(theMapHeight - 3) + 1);
+		GLfloat radius = theDistortionMap[i*theMapHeight + j];
+	
+		// uniform distribution for points
+		x = 0.5*tan((0.5*x - 0.25)*M_PI) + 0.5;
+		y = 0.5*tan((0.5*y - 0.25)*M_PI) + 0.5;
+	
+		CubeBuilder::evalVertex(vertex, x, y);
+		GLfloat length = sqrt(vertex[0] * vertex[0] + 
+				vertex[1] * vertex[1] + vertex[2] * vertex[2]);
+		GLfloat factor = radius/length;
+		vertex[0] *= factor;
+		vertex[1] *= factor;
+		vertex[2] *= factor;
+	}
+	
+	virtual void evalNormal(GLfloat (&normal)[3], GLfloat x, GLfloat y) {
+		GLfloat vertex[4][4];
+		GLfloat dx = 1.0/(theMapWidth - 3), dy = 1.0/(theMapHeight - 3);
+		
+		evalVertex(vertex[0], x - dx, y);
+		evalVertex(vertex[1], x + dx, y);
+		evalVertex(vertex[2], x, y - dy);
+		evalVertex(vertex[3], x, y + dy);
+		
+		vertex[1][0] -= vertex[0][0];
+		vertex[1][1] -= vertex[0][1];
+		vertex[1][2] -= vertex[0][2];
+		
+		vertex[3][0] -= vertex[2][0];
+		vertex[3][1] -= vertex[2][1];
+		vertex[3][2] -= vertex[2][2];
+		
+		normal[0] = vertex[1][1]*vertex[3][2] - vertex[1][2]*vertex[3][1];
+		normal[1] = - (vertex[1][0]*vertex[3][2] - vertex[1][2]*vertex[3][0]);
+		normal[2] = vertex[1][0]*vertex[3][1] - vertex[1][1]*vertex[3][0];
+	}
+	
+private:
+	GLfloat *theDistortionMap;
+	int theMapWidth;
+	int theMapHeight;
+};
+
 class MyApplication : public GlutApplication {
 public:
 	MyApplication(int argc, char *argv[]) : 
@@ -138,7 +225,7 @@ public:
 		
 		camera.setZoom(5.0);
 		
-		SphereBuilder builder(16, 16);
+		DistortedSphereBuilder builder(64, 64);
 		builder.build(model, GlModel::ALL_BUFFERS_BITS);
 	}
 	
